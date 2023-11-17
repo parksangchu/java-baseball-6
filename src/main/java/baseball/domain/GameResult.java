3dc6a682d28729d8d55f4b13aaec06b6f1c5d5e4
sangchu <didi1484@gmail.com>
2023-11-17 19:40:43 +0900

    feat: 게임 종료여부 확인 메서드 추가

--- src/main/java/baseball/domain/GameResult.java
+++ src/main/java/baseball/domain/GameResult.java
@@ -1,12 +1,17 @@
 package baseball.domain;
 
 public class GameResult {
+    private static final int GAME_END_COUNT = 3;
     private final int ball;
     private final int strike;
 
     public GameResult(int ball, int strike) {
         this.ball = ball;
         this.strike = strike;
+    }
+
+    public boolean isGameEnd() {
+        return strike == GAME_END_COUNT;
     }
 
     public int getBall() {
