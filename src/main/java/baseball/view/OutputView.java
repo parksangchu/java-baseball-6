3fd05c5ba3f5eac7af70e0ea2605aa00d499453a
sangchu <didi1484@gmail.com>
2023-11-17 16:51:58 +0900

    feat: 게임결과 출력 메서드 추가
    볼, 스트라이크의 갯수에 따른 출력 방식 결정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,7 +1,46 @@
 package baseball.view;
 
+import baseball.domain.GameResult;
+
 public class OutputView {
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+    private static final String NOTHING = "낫싱";
+    private static final String ONLY_BALL_OR_ONLY_STRIKE_FORMAT = "%d%s%n";
+    private static final String BALL_AND_STRIKE_FORMAT = "%d%s %d%s%n";
+
     public static void printError(Exception e) {
         System.out.println(e.getMessage());
+    }
+
+    public static void printGameResult(GameResult gameResult) {
+        if (isOnlyBallBy(gameResult)) {
+            System.out.printf(ONLY_BALL_OR_ONLY_STRIKE_FORMAT
+                    , gameResult.getBall(), BALL);
+            return;
+        }
+        if (isOnlyStrikeBy(gameResult)) {
+            System.out.printf(ONLY_BALL_OR_ONLY_STRIKE_FORMAT
+                    , gameResult.getStrike(), STRIKE);
+            return;
+        }
+        if (isNothingBy(gameResult)) {
+            System.out.println(NOTHING);
+            return;
+        }
+        System.out.printf(BALL_AND_STRIKE_FORMAT
+                , gameResult.getBall(), BALL, gameResult.getStrike(), STRIKE);
+    }
+
+    private static boolean isOnlyBallBy(GameResult gameResult) {
+        return gameResult.getBall() == 0 && gameResult.getStrike() != 0;
+    }
+
+    private static boolean isOnlyStrikeBy(GameResult gameResult) {
+        return gameResult.getBall() != 0 && gameResult.getStrike() == 0;
+    }
+
+    private static boolean isNothingBy(GameResult gameResult) {
+        return gameResult.getBall() == 0 && gameResult.getStrike() == 0;
     }
 }
