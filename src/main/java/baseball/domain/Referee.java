4a161440440b0a25a3e5f05303ddbc7d837005e2
sangchu <didi1484@gmail.com>
2023-11-17 18:45:52 +0900

    feat: 게임 결과 판단 메서드 추가

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -1,4 +1,34 @@
 package baseball.domain;
 
+import java.util.stream.IntStream;
+
 public class Referee {
+    public static GameResult judgeGameResultBy(Numbers computerNumbers, Numbers playerNumbers) {
+        int ball = judgeBallBy(computerNumbers, playerNumbers);
+        int strike = judgeStrikeBy(computerNumbers, playerNumbers);
+        return new GameResult(ball, strike);
+    }
+
+    private static int judgeBallBy(Numbers computerNumbers, Numbers playerNumbers) {
+        int ball = (int) IntStream.rangeClosed(0, 2)
+                .filter(index -> playerNumbers.getNumbers()
+                        .contains(computerNumbers.getNumbers()
+                                .get(index)))
+                .filter(index -> playerNumbers.getNumbers()
+                        .get(index)
+                        != computerNumbers.getNumbers()
+                        .get(index))
+                .count();
+        return ball;
+    }
+
+    private static int judgeStrikeBy(Numbers computerNumbers, Numbers playerNumbers) {
+        int strike = (int) IntStream.rangeClosed(0, 2)
+                .filter(index -> playerNumbers.getNumbers()
+                        .get(index)
+                        != computerNumbers.getNumbers()
+                        .get(index))
+                .count();
+        return strike;
+    }
 }
