8c91d728fb73c9bfd8134587043ba98323acda70
sangchu <didi1484@gmail.com>
2023-11-17 17:59:26 +0900

    test: Numbers 생성자테스트 추가

---
+++ src/test/java/baseball/domain/NumbersTest.java
@@ -0,0 +1,23 @@
+package baseball.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class NumbersTest {
+    @Test
+    @DisplayName("중복된 숫자 입력시 예외 발생")
+    void createDuplicatedNumbers() {
+        assertThatThrownBy(() -> new Numbers(List.of(1, 1, 2)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("사이즈가 3이 아닐 시 예외 발생")
+    void createInvalidSize() {
+        assertThatThrownBy(() -> new Numbers(List.of(1, 2, 3, 4)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
