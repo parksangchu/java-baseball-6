de5ed38bfc8ef5b734a01455abed13bdeedb349c
sangchu <didi1484@gmail.com>
2023-11-17 18:14:21 +0900

    feat: 문자열 -> Numbers 전환 메서드 추가

--- src/main/java/baseball/domain/Convertor.java
+++ src/main/java/baseball/domain/Convertor.java
@@ -1,4 +1,14 @@
 package baseball.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Convertor {
+    public static Numbers numbersOf(String input) {
+        List<Integer> numbers = new ArrayList<>();
+        input.chars()
+                .map(Character::getNumericValue)
+                .forEach(numbers::add);
+        return new Numbers(numbers);
+    }
 }
