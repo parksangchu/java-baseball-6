6fe52252fc97945f98d83c282f38b044c358b0de
sangchu <didi1484@gmail.com>
2023-11-17 23:17:01 +0900

    fix: 종료 메뉴 확인 인스턴스 메서드로 변경

--- src/main/java/baseball/domain/EndMenu.java
+++ src/main/java/baseball/domain/EndMenu.java
@@ -1,22 +1,27 @@
 package baseball.domain;
 
 public class EndMenu {
-    private static final String RETRY = "1";
-    private static final String COMPLETE_END = "2";
+    private static final int RETRY = 1;
+    private static final int COMPLETE_END = 2;
     private static final String INVALID_RANGE = "[ERROR] 1 혹은 2를 입력하셔야 합니다.";
+    private final int endMenu;
 
-    public static boolean isCompleteEnd(String input) {
-        validateRange(input);
-        return input.equals(COMPLETE_END);
+    public EndMenu(String endMenu) {
+        validateRange(endMenu);
+        this.endMenu = Integer.parseInt(endMenu);
     }
 
-    private static void validateRange(String input) {
+    public boolean isCompleteEnd() {
+        return endMenu == COMPLETE_END;
+    }
+
+    private void validateRange(String input) {
         if (!isValidRange(input)) {
             throw new IllegalArgumentException(INVALID_RANGE);
         }
     }
 
-    private static boolean isValidRange(String input) {
+    private boolean isValidRange(String input) {
         return input.equals(RETRY) || input.equals(COMPLETE_END);
     }
 }
