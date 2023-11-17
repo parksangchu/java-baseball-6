42b86dfc4a2684b467e0d11682d256a00c13beb7
sangchu <didi1484@gmail.com>
2023-11-17 18:59:55 +0900

    feat: 해당 인덱스의 값 일치 여부 확인 메서드 추가

--- src/main/java/baseball/domain/Numbers.java
+++ src/main/java/baseball/domain/Numbers.java
@@ -6,12 +6,12 @@ public class Numbers {
     private static final String ERROR = "[ERROR] ";
     private static final String INVALID_SIZE = ERROR + "입력하신 숫자는 3자리가 아닙니다.";
     private static final String DUPLICATED = ERROR + "중복된 숫자를 입력할 수 없습니다.";
-    private final List<Integer> Numbers;
+    private final List<Integer> numbers;
 
     public Numbers(List<Integer> numbers) {
         validateSize(numbers);
         validateDuplicated(numbers);
-        Numbers = numbers;
+        this.numbers = numbers;
     }
 
     private void validateSize(List<Integer> numbers) {
@@ -33,7 +33,11 @@ public class Numbers {
         return sizeAfterCut != numbers.size();
     }
 
+    public boolean match(List<Integer> numbers, int index) {
+        return this.numbers.get(index) == numbers.get(index);
+    }
+
     public List<Integer> getNumbers() {
-        return Numbers;
+        return numbers;
     }
 }
