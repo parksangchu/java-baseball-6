87d814c98da613c21e33b2b3f439bfadd6556b31
sangchu <didi1484@gmail.com>
2023-11-19 17:23:47 +0900

    test: 1~9 이외의 숫자 검증 메서드 추가

--- src/main/java/baseball/domain/Numbers.java
+++ src/main/java/baseball/domain/Numbers.java
@@ -6,11 +6,15 @@ public class Numbers {
     private static final String ERROR = "[ERROR] ";
     private static final String INVALID_SIZE = ERROR + "입력하신 숫자는 3자리가 아닙니다.";
     private static final String DUPLICATED = ERROR + "중복된 숫자를 입력할 수 없습니다.";
+    private static final String INVALID_RANGE = ERROR + "1~9 이외의 숫자는 입력할 수 없습니다.";
+    private static final int MAX_NUMBER = 9;
+    private static final int MIN_NUMBER = 1;
     private final List<Integer> numbers;
 
     public Numbers(List<Integer> numbers) {
         validateSize(numbers);
         validateDuplicated(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
@@ -31,6 +35,17 @@ public class Numbers {
                 .distinct()
                 .count();
         return sizeAfterCut != numbers.size();
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        if (isInvalidRange(numbers)) {
+            throw new IllegalArgumentException(INVALID_RANGE);
+        }
+    }
+
+    private boolean isInvalidRange(List<Integer> numbers) {
+        return numbers.stream()
+                .anyMatch(number -> number > MAX_NUMBER || number < MIN_NUMBER);
     }
 
     public boolean match(List<Integer> numbers, int index) {
