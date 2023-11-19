a46510fe2b1453e5061246d1c621b84096eb5c48
sangchu <didi1484@gmail.com>
2023-11-19 17:12:42 +0900

    test: 게임 종료 확인 테스트 추가

--- src/test/java/baseball/domain/GameResultTest.java
+++ src/test/java/baseball/domain/GameResultTest.java
@@ -1,13 +1,16 @@
 package baseball.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class GameResultTest {
 
     @Test
-    @DisplayName("스트라이크가 3개이면 게임이 ")
-    void isThreeStrike() {
-
+    @DisplayName("스트라이크가 3개이면 게임이 종료된다")
+    void isGameEnd() {
+        GameResult gameResult = new GameResult(0, 3);
+        assertThat(gameResult.isGameEnd()).isTrue();
     }
 }
