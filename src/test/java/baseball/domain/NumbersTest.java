a74c89b0a1364bc183b717328a4d3b6a9acbb0e2
sangchu <didi1484@gmail.com>
2023-11-19 17:27:48 +0900

    test: 1~9 이외의 숫자 검증 테스트 추가

--- src/test/java/baseball/domain/NumbersTest.java
+++ src/test/java/baseball/domain/NumbersTest.java
@@ -20,4 +20,11 @@ class NumbersTest {
         assertThatThrownBy(() -> new Numbers(List.of(1, 2, 3, 4)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    @DisplayName("1-9 이외의 숫자 입력시 예외 발생")
+    void createInvalidRange() {
+        assertThatThrownBy(() -> new Numbers(List.of(0, 1, 3)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
